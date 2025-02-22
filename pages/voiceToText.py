b9e76f930fd1ad1c3ca237014105fb752081e6ff
lijinhai255 <18854109500@163.com>
2025-02-22 19:38:34 +0800

    fix:修改类型声明

--- pages/voiceToText.py
+++ pages/voiceToText.py
@@ -1,10 +1,8 @@
-
 import streamlit as st
-from streamlit.components.v1 import html
 
-# 读取外部HTML文件
-with open("static/index.html", "r", encoding="utf-8") as f:
-    html_content = f.read()
-
-# 渲染HTML内容
-html(html_content, height=600)
+html_content = """
+<div id="test">
+  <h1>Hello World!</h1>
+</div>
+"""
+st.html(html_content, unsafe_allow_html=True)
