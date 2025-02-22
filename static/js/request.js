6e494c3d169440c5cf4777a08592bf201536fc82
lijinhai255 <18854109500@163.com>
2025-02-22 18:44:18 +0800

    fix:修改类型声明

---
+++ static/js/request.js
@@ -0,0 +1,31 @@
+const instance = axios.create({
+   
+    // baseURL: 'http://39.105.155.161:12406/',
+    // baseURL: 'http://192.168.188.11:12406/',
+    baseURL: 'http://inspur.seasite.top:12406/',
+    timeout: 60000,
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic anp5ZXM6OGtJVWl1RkI='
+    }
+})
+function get(url, params) {
+    return new Promise((resolve, reject) => {
+        console.log(instance);
+        
+        instance.get(url, { params }).then((res) => {
+            resolve(res.data);
+        }).catch((err) => {
+            reject(err.data);
+        });
+    });
+}
+function post(url, data) {
+    return new Promise((resolve, reject) => {
+        instance.post(url, data).then((res) => {
+            resolve(res.data);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
