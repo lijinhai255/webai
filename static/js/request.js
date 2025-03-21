01179c7644e3b048470f80d7b004e87577b1ca67
lijinhai255 <18854109500@163.com>
2025-03-21 08:54:55 +0800

    fix:劳工教育二维码修改

--- static/js/request.js
+++ static/js/request.js
@@ -3,6 +3,8 @@ const instance = axios.create({
   baseURL: "http://23.95.36.253:8000/",
   // baseURL: "http://localhost:8000/",
   timeout: 60000,
+  changeOrigin: true, // 改变请求的来源
+  pathRewrite: { "^/api": "" }, // 重写路径，去掉 `/api` 前缀
   headers: {
     "Content-Type": "application/json",
     Authorization: "Basic anp5ZXM6OGtJVWl1RkI=",
