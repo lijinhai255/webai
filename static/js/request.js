81e9e55a5e1cab3d142d1c5a3c558e2989574756
lijinhai255 <18854109500@163.com>
2025-03-21 08:31:17 +0800

    fix:修改

--- static/js/request.js
+++ static/js/request.js
@@ -1,7 +1,7 @@
 const instance = axios.create({
   // baseURL: 'http://39.105.155.161:12406/',
-  // baseURL: 'http://192.168.188.11:12406/',
-  baseURL: "http://localhost:8000/",
+  baseURL: "http://23.95.36.253:8000/",
+  // baseURL: "http://localhost:8000/",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
